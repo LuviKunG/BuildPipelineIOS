0baf4f9674a0811c99b48d69d2e969c7c96bf323
Thanut Panichyotai <luvikung@gmail.com>
2019-11-14 15:50:44 +0700

    initial project.

---
+++ Editor/BuildPipelineIOSSettings.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace LuviKunG.BuildPipeline.iOS
+{
+    public sealed class BuildPipelineIOSSettings
+    {
+        private const string ALIAS = "unity.editor.luvikung.buildpipeline.ios.";
+        private static readonly string PREFS_SETTINGS_BUILD_PATH = ALIAS + "buildpath";
+        private static readonly string PREFS_SETTINGS_NAME_FORMAT = ALIAS + "nameformat";
+        private static readonly string PREFS_SETTINGS_DATE_TIME_FORMAT = ALIAS + "datetimeformat";
+        private static readonly string PREFS_SETTINGS_CREATE_NEW_FOLDER = ALIAS + "createNewFolder";
+        private static readonly string PREFS_SETTINGS_SET_URL_SCHEMES = ALIAS + "setURLSchemes";
+        private static readonly string PREFS_SETTINGS_URL_SCHEMES = ALIAS + "urlSchemes";
+        private static readonly string PREFS_SETTINGS_BUILD_OPTIONS = ALIAS + "buildOptions";
+
+        public string buildPath;
+        public string nameFormat;
+        public string dateTimeFormat;
+        public bool createNewFolder;
+        public bool setURLSchemes;
+        public string[] urlSchemes;
+        public BuildOptions buildOptions;
+
+        private static BuildPipelineIOSSettings instance;
+        public static BuildPipelineIOSSettings Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new BuildPipelineIOSSettings();
+                return instance;
+            }
+        }
+
+        public BuildPipelineIOSSettings()
+        {
+            Load();
+        }
+        public void Load()
+        {
+            // Define 'BUILD_PIPELINE_IOS_UNITY_DEFAULT' if you want to set a build location as same as default of Unity Editor
+            // But it's buggy because the native doesn't generate the new folder to put a build.
+#if BUILD_PIPELINE_IOS_UNITY_DEFAULT
+            buildPath = PlayerPrefs.GetString(PREFS_SETTINGS_BUILD_PATH, EditorUserBuildSettings.GetBuildLocation(BuildTarget.iOS));
+#else
+            buildPath = PlayerPrefs.GetString(PREFS_SETTINGS_BUILD_PATH, string.Empty);
+#endif
+            nameFormat = PlayerPrefs.GetString(PREFS_SETTINGS_NAME_FORMAT, "{package}_{date}");
+            dateTimeFormat = PlayerPrefs.GetString(PREFS_SETTINGS_DATE_TIME_FORMAT, "yyyyMMddHHmmss");
+            createNewFolder = PlayerPrefs.GetString(PREFS_SETTINGS_CREATE_NEW_FOLDER, bool.TrueString) == bool.TrueString;
+            setURLSchemes = PlayerPrefs.GetString(PREFS_SETTINGS_SET_URL_SCHEMES, bool.FalseString) == bool.TrueString;
+            if (PlayerPrefs.HasKey(PREFS_SETTINGS_URL_SCHEMES))
+            {
+                string str = PlayerPrefs.GetString(PREFS_SETTINGS_URL_SCHEMES, string.Empty);
+                if (str != null && str.Length > 0)
+                    urlSchemes = str.Split('|');
+                else
+                    urlSchemes = new string[0];
+            }
+            else
+                urlSchemes = new string[0];
+            buildOptions = (BuildOptions)PlayerPrefs.GetInt(PREFS_SETTINGS_BUILD_OPTIONS, 0);
+        }
+
+        public void Save()
+        {
+            PlayerPrefs.SetString(PREFS_SETTINGS_BUILD_PATH, buildPath);
+            PlayerPrefs.SetString(PREFS_SETTINGS_NAME_FORMAT, nameFormat);
+            PlayerPrefs.SetString(PREFS_SETTINGS_DATE_TIME_FORMAT, dateTimeFormat);
+            PlayerPrefs.SetString(PREFS_SETTINGS_CREATE_NEW_FOLDER, createNewFolder ? bool.TrueString : bool.FalseString);
+            PlayerPrefs.SetString(PREFS_SETTINGS_SET_URL_SCHEMES, createNewFolder ? bool.TrueString : bool.FalseString);
+            if (urlSchemes != null && urlSchemes.Length > 0)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < urlSchemes.Length - 1; i++)
+                {
+                    sb.Append(urlSchemes[i]);
+                    sb.Append('|');
+                }
+                sb.Append(urlSchemes[urlSchemes.Length - 1]);
+                PlayerPrefs.SetString(PREFS_SETTINGS_URL_SCHEMES, sb.ToString());
+            }
+            else if (PlayerPrefs.HasKey(PREFS_SETTINGS_URL_SCHEMES))
+                PlayerPrefs.DeleteKey(PREFS_SETTINGS_URL_SCHEMES);
+            PlayerPrefs.SetInt(PREFS_SETTINGS_BUILD_OPTIONS, (int)buildOptions);
+        }
+
+        public string GetFolderName()
+        {
+            StringBuilder s = new StringBuilder(nameFormat);
+            s.Replace("{name}", Application.productName);
+            s.Replace("{package}", PlayerSettings.GetApplicationIdentifier(BuildTargetGroup.iOS));
+            s.Replace("{version}", Application.version);
+            s.Replace("{bundle}", PlayerSettings.Android.bundleVersionCode.ToString());
+            s.Replace("{date}", DateTime.Now.ToString(dateTimeFormat));
+            return s.ToString();
+        }
+    }
+}
---
+++ Editor/BuildPipelineIOSWindow.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace LuviKunG.BuildPipeline.iOS
+{
+    public sealed class BuildPipelineIOSWindow : EditorWindow
+    {
+        private const string HELPBOX_NAME_FORMATTING_INFO = @"How to format the file name.
+
+{name} = App Name.
+{package} = Android Package Name.
+{version} = App Version.
+{bundle} = App Bundle.
+{date} = Date time. (format)";
+        private static readonly GUIContent CONTENT_LIST_URL_SCHEMES_HEADER = new GUIContent("URLSchemes");
+
+        private BuildPipelineIOSSettings settings;
+        private ReorderableList listMain;
+        private List<string> urlSchemeList;
+        private bool isDirty = false;
+
+        public static BuildPipelineIOSWindow OpenWindow()
+        {
+            var window = GetWindow<BuildPipelineIOSWindow>(true, "WebGL Build Pipeline Setting", true);
+            window.Show();
+            return window;
+        }
+
+        private void OnEnable()
+        {
+            isDirty = false;
+            settings = BuildPipelineIOSSettings.Instance;
+            urlSchemeList = new List<string>();
+            listMain = new ReorderableList(urlSchemeList, typeof(string), true, true, true, true);
+            listMain.drawHeaderCallback = DrawHeader;
+            listMain.drawElementCallback = DrawElement;
+            listMain.elementHeightCallback = ElementHeight;
+            listMain.onAddCallback = OnAdd;
+            listMain.onRemoveCallback = OnRemove;
+            listMain.onReorderCallback = OnReorder;
+            //local function.
+            void DrawHeader(Rect rect)
+            {
+                EditorGUI.LabelField(rect, CONTENT_LIST_URL_SCHEMES_HEADER);
+            }
+            void DrawElement(Rect rect, int index, bool isActive, bool isFocused)
+            {
+                rect.height = EditorGUIUtility.singleLineHeight;
+                urlSchemeList[index] = EditorGUI.TextField(rect, GUIContent.none, urlSchemeList[index]);
+            }
+            float ElementHeight(int index)
+            {
+                return EditorGUIUtility.singleLineHeight;
+            }
+            void OnAdd(ReorderableList list)
+            {
+                urlSchemeList.Add(string.Empty);
+                isDirty = true;
+            }
+            void OnRemove(ReorderableList list)
+            {
+                if (list.index < 0)
+                    return;
+                urlSchemeList.RemoveAt(list.index);
+                isDirty = true;
+            }
+            void OnReorder(ReorderableList list)
+            {
+                isDirty = true;
+            }
+        }
+
+        private void OnGUI()
+        {
+            GUI.enabled = !UnityEditor.BuildPipeline.isBuildingPlayer;
+            EditorGUILayout.LabelField("Folder name formatting", EditorStyles.boldLabel);
+            using (var changeScope = new EditorGUI.ChangeCheckScope())
+            {
+                settings.nameFormat = EditorGUILayout.TextField(settings.nameFormat);
+                if (changeScope.changed)
+                    settings.Save();
+            }
+            EditorGUILayout.HelpBox(HELPBOX_NAME_FORMATTING_INFO, MessageType.Info, true);
+            EditorGUILayout.LabelField("Formatted name", settings.GetFolderName(), EditorStyles.helpBox);
+            using (var changeScope = new EditorGUI.ChangeCheckScope())
+            {
+                settings.dateTimeFormat = EditorGUILayout.TextField("Date time format", settings.dateTimeFormat);
+                settings.createNewFolder = EditorGUILayout.Toggle("Create new folder", settings.createNewFolder);
+                settings.setURLSchemes = EditorGUILayout.Toggle("Set URLSchemes", settings.setURLSchemes);
+                if (settings.setURLSchemes)
+                {
+                    if (settings.urlSchemes == null)
+                        settings.urlSchemes = new string[0];
+                    urlSchemeList.Clear();
+                    urlSchemeList.AddRange(settings.urlSchemes);
+                    listMain.DoLayoutList();
+                }
+                settings.buildOptions = (BuildOptions)EditorGUILayout.EnumFlagsField("Build options", settings.buildOptions);
+                if (changeScope.changed || isDirty)
+                {
+                    isDirty = false;
+                    if (settings.setURLSchemes)
+                        settings.urlSchemes = urlSchemeList.ToArray();
+                    settings.Save();
+                }
+            }
+            using (var verticalScope = new EditorGUILayout.VerticalScope())
+            {
+                using (var horizontalScope = new EditorGUILayout.HorizontalScope())
+                {
+                    EditorGUILayout.LabelField("Build location", string.IsNullOrWhiteSpace(settings.buildPath) ? "<No location>" : settings.buildPath);
+                    if (GUILayout.Button("Change...", GUILayout.Width(96.0f)))
+                    {
+                        var path = OpenBuildSavePanel(settings.buildPath);
+                        if (!string.IsNullOrEmpty(path))
+                            settings.buildPath = path;
+                    }
+                }
+                using (var horizontalScope = new EditorGUILayout.HorizontalScope())
+                {
+                    GUILayout.FlexibleSpace();
+                    bool cacheEnable = GUI.enabled;
+                    GUI.enabled = !string.IsNullOrWhiteSpace(settings.buildPath);
+                    if (GUILayout.Button("Open Build Location", GUILayout.MaxWidth(256.0f)))
+                    {
+                        Application.OpenURL(settings.buildPath);
+                    }
+                    GUI.enabled = cacheEnable;
+                    GUILayout.FlexibleSpace();
+                }
+            }
+        }
+
+        private string OpenBuildSavePanel(string path)
+        {
+            string newPath = EditorUtility.SaveFolderPanel("Choose Location of Build Game", path, null);
+            if (string.IsNullOrEmpty(newPath))
+                return null;
+            settings.buildPath = newPath;
+            settings.Save();
+            return newPath;
+        }
+    }
+}
